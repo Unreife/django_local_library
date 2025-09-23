ba3aadb123d70832b50242dbae2f42704a6a5a94
Oğuz İsa <140980452+Unreife@users.noreply.github.com>
2025-09-23 16:45:15 +0300

    Update settings.py
    Static root

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
